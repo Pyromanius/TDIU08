c0ea1b732322034f7584cb13c4134b6fb573ee67
Pyromanius <A.e.Gudmundsson@gmail.com>
2023-10-11 10:15:32 +0200

    Working test_exceptions

--- adao4/test_exceptions.adb
+++ adao4/test_exceptions.adb
@@ -1,35 +1,43 @@
---SUCCESS
-
-with Ada.Text_IO;         use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Strings.Fixed;   use Ada.Strings.Fixed;
+--Antgu873: Arbetat enskilt
+with Ada.Text_IO;          use Ada.Text_IO;
+with Ada.Integer_Text_IO;  use Ada.Integer_Text_IO;
+with Ada.Strings.Fixed;    use Ada.Strings.Fixed;
 
 procedure Test_Exceptions is
 
-   function Get_Correct_String(S      : in out String;
-                                 Length : in     Integer) return Boolean is
+      Length : Integer;
+      Length_Error : exception;
+
+   procedure Get_Correct_String(S : in out String) is
+
       C : Character;
-      End_Of_Line : Boolean;
-      SL : Integer;
+      End_Of_Line : Boolean := false;
+      I : Integer;
+
    begin
-      Look_Ahead(C, End_Of_Line);
-      while C = ' ' and End_Of_Line = false loop 
-         Get(C);
+      I := 1;
+      Get(C);
+
+      while C = ' ' or End_Of_Line = true loop
          Look_Ahead(C, End_Of_Line);
+         Get(C);
       end loop;
+
       S(1) := C;
-      SL := 0; 
-      for I in 2 .. Length loop
-         Get(S(I));
-         SL := SL + 1;
-         Look_Ahead(S(I), End_Of_Line);
-         if End_Of_Line = true and SL < Length then
-            return false;
-         elsif End_Of_Line = true and SL = Length then
-            return true;
-         end if;
+
+      for X in 2..Length loop
+         Look_Ahead(C, End_Of_Line);
+            if End_Of_Line = false then
+               Get(C);
+               S(X) := C;
+               I := I + 1;
+            end if;
       end loop;
-      return true;
+
+      if I < Length then
+         raise Length_Error;
+      end if;
+      
    end Get_Correct_String;
    
    ----------------------------------------------------------------------
@@ -59,7 +67,6 @@ procedure Test_Exceptions is
       return N;
    end Menu_Selection;
 
-
    
    ----------------------------------------------------------------------
    -- Underprogram för menyval 1: "felhantering av heltalsinmatning"   --
@@ -71,28 +78,32 @@ procedure Test_Exceptions is
    -- korrekt värde matas in.                                          --
    ----------------------------------------------------------------------
    procedure Upg1 is
-      
+
       Value, Min, Max : Integer;
 
    procedure Get_Safe (Value    : in out Integer;
-                       Min, Max : in     Integer) is
-                      
+                       Min, Max : in     Integer) is                   
    begin
       Put("Mata in värde (");
       Put(Min, Width=>1);
-      Put(" ");
+      Put(" - ");
       Put(Max, Width=>1);
       Put("): ");
       Get(Value);
-      if Value > Max or Value < Min then
+
+      if Value < Min then
+         Put("För litet värde. ");
+         Get_Safe(Value, Min, Max);
+      elsif Value > Max then
+         Put("För stort värde. ");
          Get_Safe(Value, Min, Max);
       end if;
-         exception 
-            when Data_Error => 
-            Put("Fel datatyp inmatat");
-            Skip_Line;
-            New_Line;
-            Get_Safe(Value, Min, Max);
+
+   exception 
+      when Data_Error => 
+         Put("Fel datatyp. ");
+         Skip_Line;
+         Get_Safe(Value, Min, Max);
 
    end Get_Safe; 
 
@@ -123,27 +134,20 @@ end Upg1;
    procedure Upg2(Length : in Integer) is
       
       S : String(1 .. Length);
-      Length_Error : exception;
       
    begin      
       Put("Mata in en sträng med exakt ");
       Put(Length, Width => 0);
       Put(" tecken: ");
       
-      if Get_Correct_String(S, Length) = false then
-         raise Length_Error;
-      end if;
-      Skip_Line;
+      Get_Correct_String(S);
       
+      Skip_Line;
       Put_Line("Du matade in strängen " & S & ".");
 
-      exception
-         when Length_Error =>
-            Put("För få inmatade tecken!");
-            New_Line;
-            Skip_Line;      
    end Upg2;
-   
+
+
    ----------------------------------------------------------------------
    -- Underprogram för menyval 3: "felhantering av datuminmatning"     --
    --                                                                  --
@@ -158,113 +162,120 @@ end Upg1;
       type Date_Type is
          record
             Year, Month, Day  : Integer;
-      end record;
+         end record;
 
       Date : Date_Type;
-      Day_Error, Month_Error, Year_Error, Length_Error, Format_Error : exception;
+      Format_Error, Day_Error, Month_Error, Year_Error : exception;
+      S : String(1..10);
 
-   procedure Put(Item : in Date_Type) is
+      procedure Put(Item : in Date_Type) is
       begin
          Put(Item.Year, Width=>0);
          Put("-");
+
          if Item.Month < 10 then
-	         Put("0");
+            Put("0");
          end if;
+
          Put(Item.Month, Width=>0);
          Put("-");
+
          if Item.Day < 10 then
-	         Put("0");
+            Put("0");
          end if;
+
          Put(Item.Day, Width=>0);
-   end Put;
+      end Put;
 
-   procedure Get(Item : out Date_Type) is
+      procedure Get(Item : in out Date_Type) is
 
-         S : String(1..10);
-
-         function SkottAr_Kontroll (Item : in Date_Type) return Boolean is         
-            begin
-               if Item.Day = 29 and Item.Month = 2 then
-                  if (Item.Year rem 4 = 0) and (Item.Year rem 100 /= 0) then
-                        return True;
-                     else
-                        return False;
-                  end if;
-               else
+         function LeapYear_Check (Item : in Date_Type) return Boolean is         
+         begin
+            if Item.Day = 29 and Item.Month = 2 then
+               if ((Item.Year rem 4 = 0) and (Item.Year rem 100 /= 0)) or Item.Year rem 400 = 0 then
                   return True;
-               end if;         
-         end SkottAr_Kontroll;
+               else
+                  return False;
+               end if;
+            else
+               return True;
+            end if;         
+         end LeapYear_Check;
 
-         procedure Dag_Kontroll(Item : in Date_Type) is 
-            begin 
-               if (Item.Day < 1 or Item.Day > 31) or ((Item.Day = 31) and (Item.Month = 4 or Item.Month = 6 or Item.Month = 9 or Item.Month = 11)) or ((SkottAr_Kontroll(Item) = false) or (Item.Month = 2 and Item.Day > 29)) then
-                  raise Day_Error;
-               end if;           
-         end Dag_Kontroll;
+         procedure Date_Check (Item : in Date_Type) is
+         begin
+            if Item.Year < 1532 or Item.Year > 9000 then
+               raise Year_Error; 
+            end if;
 
-         procedure Manad_Kontroll (Month : in Integer) is
-            begin
-               if Month < 1 or Month > 12 then
-                  raise Month_Error;
-               end if; 
-         end Manad_Kontroll;
+            if Item.Month < 1 or Item.Month > 12 then
+               raise Month_Error;
+            end if; 
 
-         procedure Ar_Kontroll (Year : in Integer) is
-            begin               
-               if Year < 1532 or Year > 9000 then
-                  raise Year_Error; 
-               end if;         
-         end Ar_Kontroll;
-
-         procedure Kontrollera_Datum (Item : in Date_Type) is
-            begin
-               Ar_Kontroll(Item.Year);
-               Manad_Kontroll(Item.Month);
-               Dag_Kontroll(Item);
-         end Kontrollera_Datum;
+            if (Item.Day < 1 or Item.Day > 31) or ((Item.Day = 31) and (Item.Month = 4 or Item.Month = 6 or Item.Month = 9 or Item.Month = 11)) or ((LeapYear_Check(Item) = false) or (Item.Month = 2 and Item.Day > 29)) then
+               raise Day_Error;
+            end if;   
+         end Date_Check;
 
       begin
-         Get(S);
-         if S'Length /= 10 then
-            raise Format_Error;
-         end if;
-            Item.Year := Integer'Value(S(1..4));
-            Item.Month := Integer'Value(S(6..7)); 
-            Item.Day := Integer'Value(S(9..10)); 
-            Kontrollera_Datum(Item);
-         if Get_Correct_String(S, 10) = false then
-            raise Format_Error;
-         end if;
-   end Get;
+         Length := 10;
 
-   begin      
+         Get_Correct_String(S);
+
+         if S(5) /= '-' or S(8) /= '-' then
+            raise Format_Error;
+         end if;
+
+         for I in 1..4 loop
+            if S(I) < '0' or S(I) > '9' then
+               raise Format_Error;
+            end if;
+         end loop;
+         
+         Item.Year := Integer'Value(S(1..4));
+
+         for I in 1..2 loop
+            if S(I+5) < '0' or S(I+5) > '9' then
+               raise Format_Error;
+            end if;
+         end loop;
+         Item.Month := Integer'Value(S(6..7));
+
+         for I in 1..2 loop
+            if S(I+8) < '0' or S(I+8) > '9' then
+               raise Format_Error;
+            end if;
+         end loop;
+         Item.Day := Integer'Value(S(9..10));
+
+         Date_Check(Item);
+
+      end Get;
+
+   begin 
       Put("Mata in ett datum: ");
       Get(Date);
-      Skip_Line;
       
       Put("Du matade in ");
       Put(Date);
       New_Line;
 
-         exception 
-      when Length_Error =>
-         raise Format_Error;
-      when Format_Error =>
-         Put("Felaktigt format! ");
+   exception
+      when Day_Error => 
+         Put("Felaktig dag! ");
+         Skip_Line; 
+         Upg3;
+      when Month_Error => 
+         Put("Felaktig månad! ");
          Skip_Line;
          Upg3;
       when Year_Error => 
          Put("Felaktigt år! ");
          Skip_Line;
          Upg3;
-      when Month_Error => 
-         Put("Felaktig månad! ");
-         Skip_Line;
-         Upg3;
-      when Day_Error => 
-         Put("Felaktig dag! "); 
-         Skip_Line;
-         Upg3;       
+      when Format_Error | Length_Error =>
+         Put("Felaktigt format! ");
+         Upg3;           
    end Upg3;
    
    ----------------------------------------------------------------------
@@ -277,7 +288,7 @@ end Upg1;
    -- av en sträng vilket skickas vidare till underporgrammet Upg2 där --
    -- strängen i sig skapas.                                           --
    ----------------------------------------------------------------------
-   Choice, Length : Integer;
+   Choice : Integer;
    
 begin 
    loop
@@ -292,7 +303,7 @@ begin
 	 Skip_Line;
 	 
 	 Upg2(Length);
-	 
+
       elsif Choice = 3 then
 	 Upg3;
 	 
@@ -301,4 +312,10 @@ begin
 	 exit;
       end if;      
    end loop;
+
+exception
+   when Length_Error =>
+      Put("För få inmatade tecken!");
+      New_Line;       
+         
 end Test_Exceptions;
