37f3b65b381915c985e0a51f2f7d28274bbd1763
Pyromanius <A.E.Gudmundsson@gmail.com>
2024-03-07 01:52:08 +0000

    Found new way to exclude in .gitignore

---
+++ adao8-11/adao81.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO;           use Ada.Text_IO;
+with Ada.Integer_Text_IO;   use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;     use Ada.Float_Text_IO;
+with Ada.Command_Line;      use Ada.Command_Line;
+
+procedure adao81 is
+    begin
+        Put(Argument_Count);
+    end adao81;
