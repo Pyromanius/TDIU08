262e5fc36fa282d480e16c0d2b2fd3e3a203ec4d
Pyromanius <A.E.Gudmundsson@gmail.com>
2024-03-07 03:56:31 +0000

    Uploading latest

--- adao8-11/adao81.adb
+++ adao8-11/adao81.adb
@@ -4,6 +4,60 @@ with Ada.Float_Text_IO;     use Ada.Float_Text_IO;
 with Ada.Command_Line;      use Ada.Command_Line;
 
 procedure adao81 is
-    begin
-        Put(Argument_Count);
-    end adao81;
+
+        procName : String := "adao81";
+
+    function check_Arg return Boolean is
+        begin
+
+        if Argument(2) = Argument(1) then
+            Put("Error! Output file """);
+            Put(Argument(2));
+            Put(""" cannot be same as input file!");
+            return false;
+        end if;
+
+        for I in 1..2 loop
+            if Argument(I) = procName or Argument(I) = ("./" & procName) then
+                if I = 1 then
+                    Put("Error! Input file""");
+                    Put(Argument(1));
+                elsif I = 2 then
+                    Put("Error! Output file """); 
+                    Put(Argument(2));
+                end if;
+
+                Put(""" cannot be same as the program itself!");
+                return false;
+            end if;
+        end loop;
+
+        if Argument_Count = 0 or Argument_Count > 2 then
+            Put("Error! Incorrect number of arguments!");
+            return false;
+        end if;
+
+        return true;
+
+    end check_Arg;
+
+
+
+begin
+
+    if check_Arg then
+        Put("Arguments okay!");
+    end if;
+
+    exception
+        when Name_Error =>
+        Put("Error! Input file """); 
+        Put(Argument(1));
+        Put(""" does not exist!");
+
+        when Status_Error =>
+        Put("Error! Output file """); 
+        Put(Argument(2));
+        Put(""" already exist!");
+
+end adao81;
