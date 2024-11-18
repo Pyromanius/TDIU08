4ff90b1fbd1d9eb444191422292697e48042c285
Pyromanius <pyromanius@protonmail.com>
2024-11-18 19:16:27 +0000

    'narrowed co3.h

--- co3/co3.h
+++ co3/co3.h
@@ -5,24 +5,17 @@
     struct run_Time{
         int run_Time_M, run_Time_S;
     };
-
     struct runner_ID {
         std::string runner_F_Name{}, runner_S_Name{}, club_Name{};
         std::vector <run_Time> runner_Times{};
     };
     
     std::vector <runner_ID> runner_List{};
-
     void Add_Runner();
-
     void Set_Runner_Time();
-
     void Print_Highscore();
-
     void Set_Runner_Time();
-
     void Rank_Time(int const runner_No, run_Time const time);
-
     void Rank_Runners();
 
 #endif
