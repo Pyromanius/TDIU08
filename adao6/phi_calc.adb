b480ad696e485f64b2ee3754444e923298cfeae7
Pyromanius <pyromanius@protonmail.com>
2024-11-18 17:28:04 +0000

    adao6; Pyramid() is now called with actual height, not H-1

--- adao6/phi_calc.adb
+++ adao6/phi_calc.adb
@@ -4,10 +4,7 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
 procedure phi_calc is
 
-        N, H : Integer;
-
     function Fib (N : in     Integer) return Integer is
-
     begin
         if N = 1 or N = 2 then
             return 1;
@@ -16,37 +13,34 @@ procedure phi_calc is
         end if;
     end Fib;
 
-    function Pyramid (H : in     integer) return Integer is
-
-        X : Integer := 1;
-
+    function Pyramid (H : in     Integer) return Integer is
+        X, N : Integer;
     begin
         if H = 0 then
             return 0;
+        else
+            N := H - 1;
+            X := 3 + (N - 2) * 2;
+            return N * X + (Pyramid(H-1));
         end if;
-
-        X := 3 + (H - 2) * 2;
-
-        return H * X + Pyramid(H - 1);
     end Pyramid;
 
+    N, H : Integer;
 begin
-    Put("Mata in N och H: ");
+    Put("Mata in H");
     Get(N);
     Get(H);
 
+    N := 1;
     Put("Fibonacci-tal nummer ");
     Put(N, Width=>1);
     Put(" är ");
     Put(Fib(N), Width=>1);
     Put(".");
-
     New_Line;
 
     Put("Det behövs ");
-    Put(Pyramid(H-1), Width=>1);
+    Put(Pyramid(H), Width=>1);
     Put(" stenar för att bygga trappan.");
-
     New_Line;
-
 end phi_calc;
