b4fb96c16a660cf4f0d35af81232aff5730c4f0f
Pyromanius <pyromanius@protonmail.com>
2024-11-19 13:45:18 +0000

    co2 code has been cleaned and corrected according to comments

--- co2/co2.cpp
+++ co2/co2.cpp
@@ -7,59 +7,34 @@
 
 using namespace std;
 
-int prog1_calc_Facul(int &n) 
+int prog1_calc_facul(int n) 
 {
-    int sum {1};
-  for (int i = 1; i < (n+1); i++)
-  {
-    sum = sum*i;   
-  }
+        int sum{1};
+    for (int i = 1; i < (n+1); i++)
+        sum = sum*i;   
 
-  return sum;
+    return sum;
 }	
 
-string prog2_multi_text(string &text, int &multiplier)
+string prog2_multi_text(string text, int multiplier)
 {
-        string multi_text;
+        string multi_text{};
     for (int i{}; i < multiplier; i++)
-    {
         multi_text += text;
-    }
 
     return multi_text; 
 }
 
-double conv_int(int &tmp)
-{
-        double conv;
-    conv = tmp;
-    return conv;
-}
-
-int conv_double(double &tmp)
-{
-        int conv;
-    conv = ceil(tmp);
-    return conv;
-}
-
 void prog3_switch_int_double(int &a, double &b)
 {
-        int tmp_1; 
-        double tmp_2;
-    tmp_1 = conv_double(b);
-    tmp_2 = conv_int(a);
+        int tmp_1{0}; 
+    tmp_1 = static_cast<int>(ceil(b));
+    b = static_cast<double>(a);
     a = tmp_1;
-    b = tmp_2;
-
 }
 
-void prog4_calc_lengths(int &total_length, double &mean_length)
+void prog4_calc_lengths(string text_1, string text_2, int &total_length, double &mean_length)
 {
-        string text_1;
-        string text_2;
-    cout << "Mata in två ord: ";
-    cin >> text_1 >> text_2;
     total_length = text_1.length() + text_2.length();
     mean_length = static_cast<double>(total_length)/2;
 }
@@ -75,52 +50,68 @@ void menu(int &sel)
         "5. Avsluta programmet." << endl << "Val: ";
         cin >> sel;
 
-        if ((sel <1) or (sel > 5))
-        {
+        if ((sel < 1) or (sel > 5))
             cout << "Fel val!" << endl;
-        }
         else
-        {
             break;
-        }
     }
+}
+
+void menu_sel_1()
+{
+        int n{0};
+    cout << "Mata in ett heltal: ";
+    cin >> n;
+    cout << "Fakulteten av " << n << " är " << prog1_calc_facul(n) << endl;
+}
+
+void menu_sel_2()
+{
+        int multiplier{0};
+        string text{};
+    cout << "Mata in en text och ett heltal: ";
+    cin >> text >> multiplier;
+    cout << "Den multiplicerade texten är " << prog2_multi_text(text, multiplier) << endl;
+}
+
+void menu_sel_3()
+{
+        int a{0};
+        double b{};
+    cout << "Mata in ett heltal och ett flyttal: ";
+    cin >> a >> b;
+    prog3_switch_int_double(a, b);
+    cout << "Heltalets värde är nu " << a << endl
+        << "Flyttalets värde är nu " << fixed << setprecision(1) << b << endl;
+}
+
+void menu_sel_4()
+{
+        int total_length{0};
+        double mean_length{};
+        string text_1{}, text_2{};
+    cout << "Mata in två ord: ";
+    cin >> text_1 >> text_2;
+    prog4_calc_lengths(text_1, text_2, total_length, mean_length);
+    cout << "Totallängd: " << total_length << endl
+        << "Medellängd: " << fixed << setprecision(1) << mean_length << endl;
 }
 
 int main()
 {
-        int sel, total_length, n, multiplier, a;
-        double mean_length, b;
-        string text;
+        int sel{0};
     cout << "Välkommen till huvudmenyn!" << endl;
     while (true)
     {   
         menu(sel);
         if (sel == 1)
-        {
-            cout << "Mata in ett heltal: ";
-            cin >> n;
-            cout << "Fakulteten av " << n << " är " << prog1_calc_Facul(n) << endl;
-        }
+            menu_sel_1();
         else if (sel == 2)
-        {
-            cout << "Mata in en text och ett heltal: ";
-            cin >> text >> multiplier;
-            cout << "Den multiplicerade texten är " << prog2_multi_text(text, multiplier) << endl;
-        }
+            menu_sel_2();
         else if (sel == 3)
-        {
-            cout << "Mata in ett heltal och ett flyttal: ";
-            cin >> a >> b;
-            prog3_switch_int_double(a, b);
-            cout << "Heltalets värde är nu " << a << endl
-                << "Flyttalets värde är nu " << fixed << setprecision(1) << b << endl;
-        }
+            menu_sel_3();
         else if (sel == 4)
-        {
-            prog4_calc_lengths(total_length, mean_length);
-            cout << "Totallängd: " << total_length << endl
-                << "Medellängd: " << fixed << setprecision(1) << mean_length << endl;
-        }
+            menu_sel_4();
         else if (sel == 5)
         {
             cout << "Ha en bra dag!";
